d8795f1b14ffa32ef99acfe139ed209e716f2b34
mixai <mixai@DESKTOP-HDIJF8C>
2017-10-05 13:17:52 +0400

    zadanie 1.1

---
+++ лаб2/zadanie11.m
@@ -0,0 +1,15 @@
+% Реалізація фільтру Хеннінга, обчислення АЧХ та ФЧХ
+fs = 1000;
+b =	[1 2 1]/4;  % коефіцієнти різницевого рівняння
+a = 1;
+n = 512;    % кількість точок, що розраховуються
+figure(1);
+[h,w] = freqz(b,a,n);   %обчислюємо АЧХ та ФЧХ
+mag = abs(h);
+phase = angle(h)*180/pi;
+%позначення осей графіків
+subplot(2,1,1), plot(w/(2*pi)*fs, mag), grid on;
+xlabel('fs'), ylabel('A');
+subplot(2,1,2), plot(w/(2*pi)*fs, unwrap(phase)), grid on;
+xlabel('fs'), ylabel('P');
+
